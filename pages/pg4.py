cacbcf9e503561b97403d7fc4eaee23027eda86a
ahmadzakys <114678611+ahmadzakys@users.noreply.github.com>
2023-05-25 14:26:49 +0700

    Add files via upload

---
+++ pages/pg4.py
@@ -0,0 +1,275 @@
+######-----Import Dash-----#####
+import dash
+from dash import dcc
+from dash import html
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
+
+import pandas as pd
+import numpy as np
+
+from data import dat_borneo, dat_celebes, dat_sumatra, dat_java, dat_dewata, dat_karimun, dat_of1, dat_natuna, dat_sumba, dat_derawan
+
+import plotly.express as px
+import plotly.graph_objects as go
+
+dash.register_page(__name__, name='Fuel Ratio') 
+
+##-----page 4 data
+
+##-----Function
+#-- 1. Volume Function
+def plot_volume(df, title):
+    fig = go.Figure()
+    fig.add_trace(go.Bar(
+        x=df['Month'].tail(4),
+        y=df['Volume Plan'].tail(4),
+        name='Plan',
+        text=df['Volume Plan'].tail(4),
+        hovertemplate='%{y:y}',
+        textangle=0,
+        marker_color='lightblue'))
+    fig.add_trace(go.Bar(
+        x=df['Month'].tail(4),
+        y=df['Volume Actual'].tail(4),
+        name='Actual',
+        text=df['Volume Actual'].tail(4),
+        hovertemplate='%{y:y}',
+        textangle=0,
+        marker_color='lightsalmon'))
+    fig.update_layout({
+        'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+        'paper_bgcolor': 'rgba(0, 0, 0, 0)',},
+        title={
+            'text': title,
+            'y':0.9,
+            'x':0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'},
+        legend={
+            'yanchor':"bottom",
+            'y':-0.25,
+            'xanchor':"center",
+            'x':0.5},
+        hovermode="x")
+
+    labels = list(df['Month'])
+    labels[-1] = "YTD'23"
+
+    fig.update_xaxes(tickvals=np.arange(4), ticktext=labels[-4:])
+    fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='#d62728', griddash='dash')
+    
+    return(fig)
+
+#-- 2. NLR Function
+def plot_nlr(df, title):
+    fig = go.Figure()
+    fig.add_trace(go.Bar(
+        x=df['Month'].tail(4),
+        y=df['NLR Plan'].tail(4),
+        name='Plan',
+        text=df['NLR Plan'].tail(4),
+        hovertemplate='%{y:y}',
+        textangle=0,
+        marker_color='lightblue'))
+    fig.add_trace(go.Bar(
+        x=df['Month'].tail(4),
+        y=df['NLR Actual'].tail(4),
+        name='Actual',
+        text=df['NLR Actual'].tail(4),
+        hovertemplate='%{y:y}',
+        textangle=0,
+        marker_color='lightsalmon'))
+    fig.update_layout({
+        'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+        'paper_bgcolor': 'rgba(0, 0, 0, 0)',},
+        title={
+            'text': title,
+            'y':0.9,
+            'x':0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'},
+        legend={
+            'yanchor':"bottom",
+            'y':-0.25,
+            'xanchor':"center",
+            'x':0.5},
+        hovermode="x")
+
+    labels = list(df['Month'])
+    labels[-1] = "AVG YTD'23"
+
+    fig.update_xaxes(tickvals=np.arange(4), ticktext=labels[-4:])
+    fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='#d62728', griddash='dash')
+    
+    return(fig)
+
+#-- 3. GLR Function
+def plot_glr(df, title):
+    fig = go.Figure()
+    fig.add_trace(go.Bar(
+        x=df['Month'].tail(4),
+        y=df['GLR Plan'].tail(4),
+        name='Plan',
+        text=df['GLR Plan'].tail(4),
+        hovertemplate='%{y:y}',
+        textangle=0,
+        marker_color='lightblue'))
+    fig.add_trace(go.Bar(
+        x=df['Month'].tail(4),
+        y=df['GLR Actual'].tail(4),
+        name='Actual',
+        text=df['GLR Actual'].tail(4),
+        hovertemplate='%{y:y}',
+        textangle=0,
+        marker_color='lightsalmon'))
+    fig.update_layout({
+        'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+        'paper_bgcolor': 'rgba(0, 0, 0, 0)',},
+        title={
+            'text': title,
+            'y':0.9,
+            'x':0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'},
+        legend={
+            'yanchor':"bottom",
+            'y':-0.25,
+            'xanchor':"center",
+            'x':0.5},
+        hovermode="x")
+
+    labels = list(df['Month'])
+    labels[-1] = "AVG YTD'23"
+
+    fig.update_xaxes(tickvals=np.arange(4), ticktext=labels[-4:])
+    fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='#d62728', griddash='dash')
+    
+    return(fig)
+
+#-- 4. Fuel Ratio Function
+def plot_fr(df, title):
+    fig = go.Figure()
+    fig.add_trace(go.Bar(
+        x=df['Month'].tail(4),
+        y=df['Fuel Ratio Gross'].tail(4),
+        name='Gross',
+        text=df['Fuel Ratio Gross'].tail(4),
+        hovertemplate='%{y:y}',
+        textangle=0,
+        marker_color='lightblue'))
+    fig.add_trace(go.Bar(
+        x=df['Month'].tail(4),
+        y=df['Fuel Ratio Net'].tail(4),
+        name='Net',
+        text=df['Fuel Ratio Net'].tail(4),
+        hovertemplate='%{y:y}',
+        textangle=0,
+        marker_color='lightsalmon'))
+    fig.update_layout({
+        'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+        'paper_bgcolor': 'rgba(0, 0, 0, 0)',},
+        title={
+            'text': title,
+            'y':0.9,
+            'x':0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'},
+        legend={
+            'yanchor':"bottom",
+            'y':-0.25,
+            'xanchor':"center",
+            'x':0.5},
+        hovermode="x")
+
+    labels = list(df['Month'])
+    labels[-1] = "AVG YTD'23"
+
+    fig.update_xaxes(tickvals=np.arange(4), ticktext=labels[-4:])
+    fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='#d62728', griddash='dash')
+    
+    return(fig)
+
+
+fr_borneo = plot_fr(dat_borneo, "Fuel Ratio Bulk Borneo")
+fr_celebes = plot_fr(dat_celebes, "Fuel Ratio Bulk Celebes")
+fr_sumatra = plot_fr(dat_sumatra, "Fuel Ratio Bulk Sumatra")
+fr_java = plot_fr(dat_java, 'Fuel Ratio Bulk Java')
+fr_dewata = plot_fr(dat_dewata, 'Fuel Ratio Bulk Dewata')
+fr_karimun = plot_fr(dat_karimun, 'Fuel Ratio Bulk Karimun')
+fr_of1 = plot_fr(dat_of1, 'Fuel Ratio Ocean Flow 1')
+fr_natuna = plot_fr(dat_natuna, 'Fuel Ratio Bulk Natuna')
+fr_sumba = plot_fr(dat_sumba, 'Fuel Ratio Bulk Sumba')
+fr_derawan = plot_fr(dat_derawan, 'Fuel Ratio Bulk Derawan')
+
+## -----LAYOUT-----
+layout = html.Div([
+                ## --ROW1--
+                dbc.Row([
+                    dbc.Col([
+                        dcc.Graph(figure=fr_borneo),
+                    ], width=4),
+                        
+                    dbc.Col([
+                        dcc.Graph(figure=fr_celebes),
+                    ], width=4),
+
+                    dbc.Col([
+                        dcc.Graph(figure=fr_sumatra),
+                    ], width=4)
+                ]),
+
+                html.Br(),
+
+                ## --ROW2--
+                dbc.Row([
+                    dbc.Col([
+                        dcc.Graph(figure=fr_java),
+                    ], width=4),
+                        
+                    dbc.Col([
+                        dcc.Graph(figure=fr_dewata),
+                    ], width=4),
+
+                    dbc.Col([
+                        dcc.Graph(figure=fr_karimun),
+                    ], width=4)
+                ]),
+
+                html.Br(),
+
+                ## --ROW3--
+                dbc.Row([
+                    dbc.Col([
+                        dcc.Graph(figure=fr_of1),
+                    ], width=4),
+                        
+                    dbc.Col([
+                        dcc.Graph(figure=fr_natuna),
+                    ], width=4),
+
+                    dbc.Col([
+                        dcc.Graph(figure=fr_sumba),
+                    ], width=4)
+                ]),
+
+                html.Br(),
+
+                ## --ROW4--
+                dbc.Row([
+                    dbc.Col([
+                        dcc.Graph(figure=fr_derawan),
+                    ], width=4),
+                ]),
+
+    html.Br(),
+    html.Footer('ABL',
+            style={'textAlign': 'center', 
+                   'fontSize': 20, 
+                   'background-color':'#242947',
+                   'color':'white'})
+
+    ], style={
+        'paddingLeft':'10px',
+        'paddingRight':'10px',
+    })
