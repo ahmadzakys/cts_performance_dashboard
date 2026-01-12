0ead4f91de7a2b3d9cdd711a63a6dbf4e3df3c92
ahmadzakys <114678611+ahmadzakys@users.noreply.github.com>
2026-01-12 17:04:05 +0700

    Update app.py
    Configure port

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import numpy as np
 import gspread
 import datetime
 
+import os
 #####-----Create a Dash app instance-----#####
 app = dash.Dash(
     __name__,
@@ -216,5 +217,10 @@ def update_data(n):
     return data_dict
 
 ######-----Start the Dash server-----#####
-if __name__ == "__main__":
-    app.run_server(debug=True)
+# if __name__ == "__main__":
+
+#     app.run_server(debug=True)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8050))
+    app.run_server(debug=False, host='0.0.0.0', port=port)
